ce92375eba25cdaf008d507bc5af158e1025bd5f
Rubén Santana Lorenzo <rubensanlor@gmail.com>
2021-11-26 14:17:27 +0000

    Implementing Meeting and MeetingPoint classes

--- model/src/main/java/model/Meeting.java
+++ model/src/main/java/model/Meeting.java
@@ -1,4 +1,16 @@
 package model;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Meeting {
+    private final ArrayList<Delegate> participants;
+    private final ArrayList<MeetingPoint> meetingPoints;
+    private final LocalDate date;
+
+    public Meeting() {
+        date = LocalDate.now();
+        participants = new ArrayList<>();
+        meetingPoints = new ArrayList<>();
+    }
 }
--- model/src/main/java/model/MeetingPoint.java
+++ model/src/main/java/model/MeetingPoint.java
@@ -4,12 +4,32 @@ public class MeetingPoint {
     private final String description;
     private final Delegate speaker;
     private final InterventionPriorityQueue interventionQueue;
+    private Intervention currentIntervention;
 
     public MeetingPoint(String description, Delegate speaker) {
         this.description = description;
         this.speaker = speaker;
         interventionQueue = new InterventionPriorityQueue();
+        currentIntervention = null;
     }
 
+    public Intervention getCurrentIntervention(){
+        return currentIntervention;
+    }
 
+    public void addIntervention(Intervention newIntervention){
+        interventionQueue.push(newIntervention);
+    }
+
+    public void cancelIntervention(Intervention canceledIntervention){
+        interventionQueue.remove(canceledIntervention);
+    }
+
+    public void endCurrentIntervention(){
+        currentIntervention = null;
+    }
+
+    public void passIntervention() {
+        currentIntervention = interventionQueue.pop();
+    }
 }
